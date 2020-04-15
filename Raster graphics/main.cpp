3292f4d1290df1b0632764e97be9546d4cffec51
mar7yk <martykostov@gmail.com>
2020-04-15 16:28:36 +0300

    check

---
+++ Raster graphics/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Raster graphics
+//
+//  Created by Marty Kostov on 15.04.20.
+//  Copyright © 2020 Marty Kostov. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+    
+    return 0;
+}

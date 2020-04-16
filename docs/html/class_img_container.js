f7e5fa5810409ffbcac42de98d792eb1884a381c
mar7yk <martykostov@gmail.com>
2020-04-16 23:24:18 +0300

    del

--- docs/html/class_img_container.js
+++
@@ -1,7 +0,0 @@
-var class_img_container =
-[
-    [ "ImgContainer", "class_img_container.html#a5981ead5b7ab92fbf22000cdbde3a74e", null ],
-    [ "~ImgContainer", "class_img_container.html#aae5b1ea09393376745c8eabae52c94ca", null ],
-    [ "add", "class_img_container.html#af13ceac828bd2b214cbe7dd265d592fc", null ],
-    [ "operator[]", "class_img_container.html#a537cf2bb54d16dd50fca229d3b62e1a0", null ]
-];
--- docs/html/class_str_command_interpr.js
+++
@@ -1,4 +0,0 @@
-var class_str_command_interpr =
-[
-    [ "execut", "class_str_command_interpr.html#a8e351f5e615ea603eda2b8841f59657e", null ]
-];

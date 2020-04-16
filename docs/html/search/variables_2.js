8ef5697c0bf3bfebc54f5aadc3012997f467a243
mar7yk <martykostov@gmail.com>
2020-04-17 02:56:37 +0300

    docs

--- docs/html/search/variables_2.js
+++ docs/html/search/variables_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['g_239',['g',['../structpixel___r_g_b.html#ae4c8b662abe9e72cfb3e0fece35396e2',1,'pixel_RGB']]]
+  ['data_301',['data',['../struct_instr_container_1_1inst.html#af149bd56caebb93c47b16d6b3da909db',1,'InstrContainer::inst::data()'],['../struct_session_container_1_1_element.html#a4dad30c6cdc78d1354ff8ad7952088d6',1,'SessionContainer::Element::data()']]]
 ];

149f1ec88546d18b91f240c7da074a613b3a17b9
mar7yk <martykostov@gmail.com>
2020-04-17 02:57:07 +0300

    docs

--- docs/html/search/functions_8.js
+++ docs/html/search/functions_8.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['negative_196',['negative',['../class_image.html#aff53edc2799e9be84ebe3c565c4f0697',1,'Image::negative()'],['../class_image_p_b_m.html#a964b2686ad93294c18eb6b66fbfd0f82',1,'ImagePBM::negative()'],['../class_image_p_g_m.html#a88a169d1db9f91a1ef485caaabaa59b2',1,'ImagePGM::negative()'],['../class_image_p_p_m.html#a02d3760af67bcb666c9ed0f3a6ef8fae',1,'ImagePPM::negative()']]]
+  ['main_249',['main',['../main_8cpp.html#ae66f6b31b5ad750f1fe042a706a4e3d4',1,'main.cpp']]],
+  ['makenew_250',['makeNew',['../class_session_container.html#aaf2eaa22a1a31badfa16a8ad44fd0ab7',1,'SessionContainer']]],
+  ['monochrome_251',['monochrome',['../class_image.html#a5c123189b0ed2d31e8a80e05696a8412',1,'Image::monochrome()'],['../class_image_p_b_m.html#a9057fe27039d6b1c273e088767b5c168',1,'ImagePBM::monochrome()'],['../class_image_p_g_m.html#a7a497a7b8dda0e19f1274f6a80f277a9',1,'ImagePGM::monochrome()'],['../class_image_p_p_m.html#a5caa9a2482ab34348da52cf593906868',1,'ImagePPM::monochrome()']]]
 ];

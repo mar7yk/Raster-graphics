7da082bb53e0b3822674ee578799311a89aaf7cc
mar7yk <martykostov@gmail.com>
2020-04-17 02:56:44 +0300

    docs

--- docs/html/search/all_12.js
+++ docs/html/search/all_12.js
@@ -1,10 +1,10 @@
 var searchData=
 [
-  ['_7eimage_118',['~Image',['../class_image.html#a46561cd7dcf7e203e9579737e8ace4eb',1,'Image']]],
-  ['_7eimagecontainer_119',['~ImageContainer',['../class_image_container.html#af57f7eda4a80068d797ad33b37ca8ae1',1,'ImageContainer']]],
-  ['_7eimgcontainer_120',['~ImgContainer',['../class_img_container.html#aae5b1ea09393376745c8eabae52c94ca',1,'ImgContainer']]],
-  ['_7einstrcontainer_121',['~InstrContainer',['../class_instr_container.html#a725bccacf730f924863100c19ecfd583',1,'InstrContainer']]],
-  ['_7epixelmatrix_122',['~PixelMatrix',['../class_pixel_matrix.html#afc0ef200c57b0b23fbbf5941afe4bbfe',1,'PixelMatrix']]],
-  ['_7esessioncontainer_123',['~SessionContainer',['../class_session_container.html#a79dbffb236f074495a258a1f8a4c5363',1,'SessionContainer']]],
-  ['_7estring_124',['~String',['../class_string.html#ac40b2a3fb58c2d8556f5e6ff73510036',1,'String']]]
+  ['width_150',['width',['../class_pixel_matrix.html#a79c5f123f28f413cdd7ef9bc6dfa492c',1,'PixelMatrix']]],
+  ['writepbm_151',['writePBM',['../class_file_interpr.html#a4df5a5d538b72b66f14f99304fa482f2',1,'FileInterpr']]],
+  ['writepbmas_152',['writePBMas',['../class_file_interpr.html#ad5ebbc07c699bed0a6ee3281be44c7da',1,'FileInterpr']]],
+  ['writepgm_153',['writePGM',['../class_file_interpr.html#a293de3f6f69cf36e9c1ea0f1b39aebb1',1,'FileInterpr']]],
+  ['writepgmas_154',['writePGMas',['../class_file_interpr.html#a36ca5fea3878c7ba5378666e9cbcec61',1,'FileInterpr']]],
+  ['writeppm_155',['writePPM',['../class_file_interpr.html#a99c53dbd57d66fe25807bef9d73fb0b0',1,'FileInterpr']]],
+  ['writeppmas_156',['writePPMas',['../class_file_interpr.html#a2724fb954d14da8adc893c33659b668f',1,'FileInterpr']]]
 ];
--- docs/html/search/functions_5.js
+++ docs/html/search/functions_5.js
@@ -1,13 +1,4 @@
 var searchData=
 [
-  ['get_176',['get',['../class_string.html#a5d05517bcc8df58bc5eff312e3926b61',1,'String']]],
-  ['gethight_177',['getHight',['../class_pixel_matrix.html#ab269198ed7affb629063d50a0d198797',1,'PixelMatrix']]],
-  ['getid_178',['getID',['../class_session.html#ac98e3c3e0a14d4f2348b8b74adaa73d3',1,'Session']]],
-  ['getline_179',['getline',['../string_8cpp.html#ab8d2f1060b05bc5dbdc32233ef0c81c8',1,'string.cpp']]],
-  ['getname_180',['getName',['../class_image.html#a1adafa7d96166cded499b5dcdacebe65',1,'Image']]],
-  ['getnamesofimgs_181',['getNamesOfImgs',['../class_session.html#a4bdb9e9bd3554fbfeb3b186900434b1a',1,'Session']]],
-  ['getnamesofinstr_182',['getNamesOfInstr',['../class_session.html#ac5e60ec471673dba663c4dd22592baf1',1,'Session']]],
-  ['getsize_183',['getSize',['../class_image_container.html#aaa43e4899b35541d8aa26c26dd1a116c',1,'ImageContainer']]],
-  ['getwidth_184',['getWidth',['../class_pixel_matrix.html#ac7fed9a9e738fd87a641f1c97d142403',1,'PixelMatrix']]],
-  ['grayscale_185',['grayscale',['../class_image.html#af6d40be234c94a5d3d562c8475aedd6f',1,'Image::grayscale()'],['../class_image_p_b_m.html#a292f5f4fadf518ae92ffd0975d003a3f',1,'ImagePBM::grayscale()'],['../class_image_p_g_m.html#a4fb1cde4db93b8997ec3b63fba39635d',1,'ImagePGM::grayscale()'],['../class_image_p_p_m.html#aebf2cd4c488b9b3e8627e1fb877a9e12',1,'ImagePPM::grayscale()']]]
+  ['front_228',['front',['../class_instr_container.html#ae842816280e4bce18e418770b80cb34f',1,'InstrContainer']]]
 ];

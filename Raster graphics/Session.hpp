104baf1f9029795f8ddb706ffdd1d98e377d791a
mar7yk <martykostov@gmail.com>
2020-06-09 19:26:42 +0300

    change

--- Raster graphics/Session.hpp
+++ Raster graphics/Session.hpp
@@ -19,7 +19,7 @@
 #include "Command.h"
 #include "ImageType.h"
 
-#include "FileInterpreter.hpp"
+#include "ImageFileInterpreter.hpp"
 
 class Session {
     static size_t nextID;
@@ -32,16 +32,46 @@ public:
     Session();
     
     size_t getID() const;
+    
     Vector<String> getNamesOfImgs() const;
+    
+    /// @return Вектор от имената на неизпълнените инструкции
     Vector<String> getNamesOfInstr() const;
     
+    
+    /// Добавя снимка в сесията
+    ///
+    /// @param name име на снимка
+    /// @return добавена ли е сниката
     bool add(const String& name);
-    void addCollage(const String& img1Name, const String& img2Name, const String& name, const Command type);
+    
+    /// Добавя колаж към синките в сесията
+    ///
+    /// @param img1Name - име на пурвата снимка
+    /// @param img2Name - име на втората снимка
+    /// @param name          - име на колажа
+    /// @param type          - тип на колажа (CollageHorizontal или CollageVertical)
+    void addCollage(const String& img1Name, const String& img2Name,
+                    const String& name, const Command type);
+    
+    /// Добавя комнда към списъка с комнди на сесията
     void addCommand(const Command command);
     
+    
+    /// Запазва всички снимки
+    ///
+    /// Изпълнява командите от списъка с комнди вурху всички снимки в сесията и след това ги запазва.
+    /// След изпулнението на всяка команда тя се изтрива от списъка.
     void save();
+    
+    /// Запазва първата снимка в сесията под ново име
+    ///
+    /// Изпълнява командите от списъка с комнди вурху снимката.
+    /// След изпулнението на всяка команда тя се изтрива от списъка.
     void saveAs(const String name);
     
+    
+    /// Истрива поскедната команда от списъка с команди
     void undo();
 };
 

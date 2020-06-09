67159d8a9abbc017f1459a9e33506ff31b3537b3
mar7yk <martykostov@gmail.com>
2020-06-09 20:22:45 +0300

    update docs

---
+++ docs/html/struct_instruction_list_1_1inst.js
@@ -0,0 +1,5 @@
+var struct_instruction_list_1_1inst =
+[
+    [ "data", "struct_instruction_list_1_1inst.html#ab7ec79ee537027c1f9c7821b9ae42ad2", null ],
+    [ "next", "struct_instruction_list_1_1inst.html#acede73788d5123583fea22f5e2e64183", null ]
+];
---
+++ docs/html/struct_session_list_1_1_element.js
@@ -0,0 +1,5 @@
+var struct_session_list_1_1_element =
+[
+    [ "data", "struct_session_list_1_1_element.html#adfa48b2375f7fb94d9f4568740e99c02", null ],
+    [ "next", "struct_session_list_1_1_element.html#a0e1c2e2c64e9fddbe7dc41507690ac91", null ]
+];
